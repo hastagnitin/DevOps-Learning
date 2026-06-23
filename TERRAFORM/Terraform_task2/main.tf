7341c5af53f15b308b45fb26c0509e03ce645f18
hastagnitin <nitin978032@gmail.com>
2026-06-23 21:58:30 +0530

    feat(terraform): update infrastructure configuration in main.tf

--- TERRAFORM/Terraform_task2/main.tf
+++ TERRAFORM/Terraform_task2/main.tf
@@ -1,9 +1,8 @@
-resource "aws_instance" "OS" {
-
-   ami = var.ami
-   instance_type = var.instance_type
-   tags = {
-       
-            Name = var.instance_name
-     }
+terraform {
+  backend "s3" {
+    bucket         = "my-devops-state-bucket" 
+    key            = "terraform/task2/state.tfstate"
+    region         = "ap-south-1"
+    
+  }
 }
