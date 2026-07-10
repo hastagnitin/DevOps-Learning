282d32347f27148cbac8188f28192457f8b9c1cb
Nitin Gupta <nitin978032@gmail.com>
2026-07-10 18:30:25 +0530

    Delete Docker directory

--- Docker/TERRAFORM/Terraform_task2/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-devops-state-bucket" 
-    key            = "terraform/task2/state.tfstate"
-    region         = "ap-south-1"
-    
-  }
-}
--- Docker/devops-scripts/service-checker.sh
+++
@@ -1,4 +0,0 @@
-echo "=== Service Status Check ==="
-echo "SSH: $(systemctl is-active sshd)"
-echo "Firewall: $(systemctl is-active firewalld)"
-echo "System Logging: $(systemctl is-active rsyslog)"
