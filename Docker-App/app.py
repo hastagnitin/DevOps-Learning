89915d4bc6fa9499ac5ce62f9533f22d0df619ee
hastagnitin <nitin978032@gmail.com>
2026-07-13 20:35:51 +0530

    updated Docker-App

--- Docker-App/app.py
+++ Docker-App/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! Welcome to your first Flask app."
+    return "Hello World! Welcome to  Flask app."
 
 @app.route('/about')
 def about():
