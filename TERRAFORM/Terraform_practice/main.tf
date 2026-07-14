c754f31d962dc1bdc835560de3d582972416135d
hastagnitin <nitin978032@gmail.com>
2026-07-14 19:40:34 +0530

    created vpc by using terraform

--- TERRAFORM/Terraform_practice/main.tf
+++ TERRAFORM/Terraform_practice/main.tf
@@ -1,3 +1,29 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 6.0"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   region = "ap-south-1"
+
+# }
+
+# resource "aws_s3_bucket" "HCL-Bucket" {
+#   bucket = "aws-create-bucket-1996"
+
+#   tags = {
+#     Name = "Terraform-S3"
+#     ENV  = "DEV"
+#   }
+
+# }
+
+
+
 terraform {
   required_providers {
     aws = {
@@ -12,12 +38,13 @@ provider "aws" {
 
 }
 
-resource "aws_s3_bucket" "HCL-Bucket" {
-  bucket = "aws-create-bucket-1996"
+resource "aws_vpc" "HCL-vpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    Name = "Terraform-S3"
-    ENV  = "DEV"
+    Name = "Terraform-VPC"
+   
   }
 
 }
