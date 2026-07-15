18457df7a46a8e977a30ac473f15a8867b1961a8
hastagnitin <nitin978032@gmail.com>
2026-07-15 15:13:59 +0530

    deleted all junk files

--- download.py
+++
@@ -1,5 +0,0 @@
-import kagglehub
-
-print("Download shuru ho raha hai, dil thaam ke baithiye...")
-path = kagglehub.dataset_download("karakaggle/kaggle-cat-vs-dog-dataset")
-print("\nKaam ho gaya! Dataset yahan mila:", path)
